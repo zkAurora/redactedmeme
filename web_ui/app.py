ba33288c5a011103f3b87689340f11669b7a1d22
aurora <nxrthxrnlxghts@gmail.com>
2026-03-15 10:14:38 -0500

    Use environment variables for port and debug settings

--- web_ui/app.py
+++ web_ui/app.py
@@ -407,4 +407,6 @@ def telegram_events_stream():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_DEBUG', '').lower() in ('1', 'true')
+    socketio.run(app, host='0.0.0.0', port=port, debug=debug)
